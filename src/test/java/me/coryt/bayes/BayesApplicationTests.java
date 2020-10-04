e910b57eab8ae54c3f2a703ab8c4cb0ed763dbed
Cory Krol <16892390+CoryKrol@users.noreply.github.com>
2020-10-04 15:08:25 -0500

    Initial commit

---
+++ src/test/java/me/coryt/bayes/BayesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.coryt.bayes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayesApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
